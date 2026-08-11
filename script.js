2589912daf2a8f87d00d64289d4f3847b02eb830
rafaelamartinszanoni-ux <rafaela.martins.zanoni@escola.pr.gov.br>
2026-08-11 20:05:43 -0300

    23

--- script.js
+++ script.js
@@ -1,40 +1,11 @@
 let curtidas = 0;
 
-const botaoCurtir = document.getElementById("curtir");
+const botao = document.getElementById("curtir");
 const contador = document.getElementById("contador");
 
-botaoCurtir.addEventListener("click", function () {
+botao.onclick = function() {
+        curtidas = curtidas + 1;
+            contador.textContent = "❤️ " + curtidas + " curtidas";
+};
 
-    curtidas++;
-
-        contador.innerHTML = "❤️ " + curtidas + " curtidas";
-
-        }); 
-
-
-        const curiosidades = [
-
-                "💜 Olivia Rodrigo nasceu em 20 de fevereiro de 2003.",
-
-                    "🎤 Drivers License foi um dos maiores sucessos de sua carreira.",
-
-                        "🎵 Olivia participa da composição de suas próprias músicas.",
-
-                            "🎬 Antes da carreira musical, Olivia também ficou conhecida como atriz.",
-
-                                "💿 Cada álbum representa uma fase diferente de sua carreira."
-
-                                ];
-
-                                const botaoCuriosidade = document.getElementById("curiosidade");
-
-                                const textoCuriosidade = document.getElementById("textoCuriosidade");
-
-                                botaoCuriosidade.addEventListener("click", function () {
-
-                                    let numero = Math.floor(Math.random() * curiosidades.length);
-
-                                        textoCuriosidade.innerHTML = curiosidades[numero];
-
-                                        });
         
