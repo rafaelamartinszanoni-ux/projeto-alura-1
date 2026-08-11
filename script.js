ce65451542cdf6628bca173acc38723ec96243d3
rafaelamartinszanoni-ux <rafaela.martins.zanoni@escola.pr.gov.br>
2026-08-11 18:37:07 -0300

    19

--- script.js
+++ script.js
@@ -9,32 +9,32 @@ botaoCurtir.addEventListener("click", function () {
 
         contador.innerHTML = "❤️ " + curtidas + " curtidas";
 
-        });
+        }); 
 
 
         const curiosidades = [
 
-            "💜 Olivia Rodrigo nasceu em 20 de fevereiro de 2003.",
+                "💜 Olivia Rodrigo nasceu em 20 de fevereiro de 2003.",
 
-                "🎤 Drivers License ficou semanas no topo das paradas musicais.",
+                    "🎤 Drivers License foi um dos maiores sucessos de sua carreira.",
 
-                    "🎵 Olivia escreve muitas das próprias músicas.",
+                        "🎵 Olivia participa da composição de suas próprias músicas.",
 
-                        "🎬 Antes da música, Olivia também ficou conhecida como atriz.",
+                            "🎬 Antes da carreira musical, Olivia também ficou conhecida como atriz.",
 
-                            "💿 Cada álbum representa uma fase diferente da vida dela."
+                                "💿 Cada álbum representa uma fase diferente de sua carreira."
 
-                            ];
+                                ];
 
+                                const botaoCuriosidade = document.getElementById("curiosidade");
 
-                            const botaoCuriosidade = document.getElementById("curiosidade");
-                            const textoCuriosidade = document.getElementById("textoCuriosidade");
+                                const textoCuriosidade = document.getElementById("textoCuriosidade");
 
+                                botaoCuriosidade.addEventListener("click", function () {
 
-                            botaoCuriosidade.addEventListener("click", function () {
+                                    let numero = Math.floor(Math.random() * curiosidades.length);
 
-                                let numero = Math.floor(Math.random() * curiosidades.length);
+                                        textoCuriosidade.innerHTML = curiosidades[numero];
 
-                                    textoCuriosidade.innerHTML = curiosidades[numero];
-
-                                    });
+                                        });
+        
