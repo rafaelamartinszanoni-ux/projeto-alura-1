7614fb96c7ffb5f51de693ddea4bb2e2f3c8ad74
rafaelamartinszanoni-ux <rafaela.martins.zanoni@escola.pr.gov.br>
2026-08-12 07:43:04 -0300

    24

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ botao.onclick = function() {
             contador.textContent = "❤️ " + curtidas + " curtidas";
 };
 
-        
+function botaoClicado() {
+console.log("fui clicado");
+let texto = botao.querySelector("span");
+
+if (curtiu === false) {
+texto.textContent++;
+curtiu = true;
+} else {
+texto.textContent--;
+curtiu = false;
+}
+}         
